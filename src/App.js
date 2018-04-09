63038cc4a853ab7876ff7886dffdafcb154a3b22
jonilsen <jonilsen@gmail.com>
2018-04-09 13:52:24 -0400

    Added basic theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colorGreen: "#2e8540",
+  colorGreenLight: "#4aa564"
+};
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
+      <ThemeProvider theme={theme}>
+        <div>
+          <h1>Hello World</h1>
+        </div>
+      </ThemeProvider>
     );
   }
 }
