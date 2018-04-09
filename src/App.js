2bc5539ad2c0636677770b4aa32f545a191a7504
jonilsen <jonilsen@gmail.com>
2018-04-09 14:17:18 -0400

    Added more theme styles and created basic alert component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from "react";
 import { ThemeProvider } from "styled-components";
+import Alert from "./components/alert/alert";
 
 const theme = {
+  colorGrayLightest: "#f1f1f1",
   colorGreen: "#2e8540",
-  colorGreenLight: "#4aa564"
+  colorGreenLightest: "#e7f4e4",
+  colorGold: "#fdb81e",
+  colorGoldLightest: "#fff1d2 ",
+  colorSecondary: "#e31c3d ",
+  colorSecondaryLightest: "#f9dede",
+  colorPrimaryAlt: "#02bfe7",
+  colorPrimaryAltLightest: "#e1f3f8",
+  spacingMedium: "2rem",
+  spacingSmall: "1rem",
+  iconSize: "3rem"
 };
 
 class App extends Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
-        <div>
-          <h1>Hello World</h1>
-        </div>
+        <Alert />
       </ThemeProvider>
     );
   }
---
+++ src/components/alert/alert.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+//Create a new Todo item
+class Alert extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h3>Success Status</h3>
+          <p>
+            Lorem ipsum dolor sit amet,{" "}
+            <a href="javascript:void(0);">consectetur adipiscing</a> elit, sed
+            do eiusmod.
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Alert;
