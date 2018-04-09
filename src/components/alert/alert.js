87e968b7105955152b3241930851a6c309c50a40
jonilsen <jonilsen@gmail.com>
2018-04-09 14:54:16 -0400

    Added the first styled component

--- src/components/alert/alert.js
+++ src/components/alert/alert.js
@@ -1,10 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+
+const UsaAlert = styled.div`
+  background-color: ${props => props.theme.colorGrayLightest};
+  background-position: ${props => props.theme.spacingMedium}
+    ${props => props.theme.spacingMedium};
+  background-repeat: no-repeat;
+  background-size: 5.2rem;
+  padding-bottom: 1.4rem;
+  padding-left: 3rem;
+  padding-right: ${props => props.theme.spacingMedium};
+  padding-top: ${props => props.theme.spacingMedium};
+  position: relative;
+`;
 
 //Create a new Todo item
 class Alert extends React.Component {
   render() {
     return (
-      <div>
+      <UsaAlert>
         <div>
           <h3>Success Status</h3>
           <p>
@@ -13,7 +27,7 @@ class Alert extends React.Component {
             do eiusmod.
           </p>
         </div>
-      </div>
+      </UsaAlert>
     );
   }
 }
